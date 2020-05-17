77286627e1138e0902f1893e230c27bf626a4fab
Holger Nestmann <hnestmann@salesforce.com>
2020-05-17 09:36:03 +0200

    Recovering SFRA look

--- cartridges/app_storefront_base/cartridge/scripts/theming/skin.js
+++ cartridges/app_storefront_base/cartridge/scripts/theming/skin.js
@@ -6,13 +6,16 @@ var ContentMgr = require('dw/content/ContentMgr');
 exports.renderSkin = function renderSkin() {
     var contentAsset = ContentMgr.getContent('store-skin');
     var renderParameters = new HashMap();
-    if (contentAsset.custom.customCSSFile) {
-        renderParameters.type = 'FILE';
-        renderParameters.url = contentAsset.custom.customCSSFile.getURL();
-    } else {
-        renderParameters.type = 'INLINE';
-        renderParameters.markup = contentAsset.custom.body;
+    if (contentAsset) {
+        if (contentAsset.custom.customCSSFile) {
+            renderParameters.type = 'FILE';
+            renderParameters.url = contentAsset.custom.customCSSFile.getURL();
+        } else {
+            renderParameters.type = 'INLINE';
+            renderParameters.markup = contentAsset.custom.body;
+        }
+        var template = new Template('components/skin');
+        return template.render(renderParameters).text;
     }
-    var template = new Template('components/skin');
-    return template.render(renderParameters).text;
+    return '';
 };
