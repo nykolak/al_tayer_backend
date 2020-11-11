ab5d770279748a836ffc9f18df250b906fde2cc4
Jason Poblador <jpoblador@salesforce.com>
2020-11-11 17:39:18 -0500

    ContactUs, CSRF, and Default controllers completed

--- cartridges/app_storefront_base/cartridge/controllers/CSRF.js
+++ cartridges/app_storefront_base/cartridge/controllers/CSRF.js
@@ -1,14 +1,34 @@
 'use strict';
 
+/**
+ * @namespace CSRF
+ */
+
 var server = require('server');
 
 var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 
+/**
+ * CSRF-Fail : The CSRF-Fail endpoint is responsible for rendering the CSRF token mismatch error page
+ * @name Base/CSRF-Fail
+ * @function
+ * @memberof CSRF
+ * @param {category} - non-sensitive
+ * @param {serverfunction} - get
+ */
 server.get('Fail', function (req, res, next) {
     res.render('/csrfFail');
     next();
 });
 
+/**
+ * CSRF-AjaxFail : The CSRF-AjaxFail endpoint is responsible for handling CSRF token mismatch in ajax requests
+ * @name Base/CSRF-AjaxFail
+ * @function
+ * @memberof CSRF
+ * @param {category} - non-sensitive
+ * @param {serverfunction} - get
+ */
 server.get('AjaxFail', function (req, res, next) {
     var URLUtils = require('dw/web/URLUtils');
     res.setStatusCode(500);
@@ -16,6 +36,14 @@ server.get('AjaxFail', function (req, res, next) {
     next();
 });
 
+/**
+ * CSRF-Generate : This endpoint generates a CSRF token... This is made for integration test purposes
+ * @name Base/CSRF-Generate
+ * @function
+ * @memberof CSRF
+ * @param {category} - non-sensitive
+ * @param {serverfunction} - get
+ */
 server.post('Generate', csrfProtection.generateToken, function (req, res, next) {
     res.json();
     next();
