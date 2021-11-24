cdd920cfb4f9b54cc7a73def61e367162bf777a8
Andreas Seyfarth <aseyfarth@salesforce.com>
2021-11-24 15:39:03 +0100

    fix for promotion cache issue W-7742735
    - fix and unit test for W-7742735

--- cartridges/app_storefront_base/cartridge/scripts/util/promotionCache.js
+++ cartridges/app_storefront_base/cartridge/scripts/util/promotionCache.js
@@ -1,4 +1,4 @@
-/* globals session */
+/* globals session request */
 'use strict';
 
 var PromotionMgr = require('dw/campaign/PromotionMgr');
@@ -8,15 +8,22 @@ var promotionCache = Object.create(null);
 
 Object.defineProperty(promotionCache, 'promotions', {
     get: function () {
+        var cacheKey = request.requestID.split('-')[0];
         if (session.privacy.promoCache) {
-            return JSON.parse(session.privacy.promoCache);
+            var cacheObj = JSON.parse(session.privacy.promoCache);
+            if (cacheKey === cacheObj.cacheKey) {
+                return cacheObj.promoIds;
+            }
         }
         var activePromotions = PromotionMgr.activeCustomerPromotions.getProductPromotions();
         var promoIds = collections.map(activePromotions, function (promo) {
             return promo.ID;
         });
 
-        session.privacy.promoCache = JSON.stringify(promoIds);
+        session.privacy.promoCache = JSON.stringify({
+            cacheKey: cacheKey,
+            promoIds: promoIds
+        });
         return promoIds;
     }
 });
