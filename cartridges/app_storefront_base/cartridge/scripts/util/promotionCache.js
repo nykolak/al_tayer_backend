a24f85440d35c85e12eb24d5c40520aec1a2324b
Andreas Seyfarth <aseyfarth@salesforce.com>
2021-11-26 19:59:37 +0100

    add comment on cache usage

--- cartridges/app_storefront_base/cartridge/scripts/util/promotionCache.js
+++ cartridges/app_storefront_base/cartridge/scripts/util/promotionCache.js
@@ -8,6 +8,9 @@ var promotionCache = Object.create(null);
 
 Object.defineProperty(promotionCache, 'promotions', {
     get: function () {
+        // will parse the id from the main request and use that as cache key for the promotion cache as the main goal is to ensure that
+        // the promotions are just calculated once for the main request (Search) and not all the remote includes.
+        // If a new main request comes in it should not be used as eventually applicable promotions have been changed linked to the user.
         var cacheKey = request.requestID.split('-')[0];
         if (session.privacy.promoCache) {
             var cacheObj = JSON.parse(session.privacy.promoCache);
